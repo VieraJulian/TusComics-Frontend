bbac9fd5ac12290ff1735ce57e73a131d42a27ed
VieraJulian <julianviera2019@hotmail.com>
2022-12-15 19:15:24 -0300

    Footer.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './pages/Navbar'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import Footer from './pages/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
+      <Footer />
     </>
   )
 }
---
+++ src/pages/Footer.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import '../../public/css/Footer-mobile.css'
+import '../../public/css/Footer-desktop.css'
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <Link to="/">INICIO</Link>
+                <Link to="/">COMICS</Link>
+                <Link to="/">FIGURAS</Link>
+                <picture>
+                    <Link to='/'><img src="/public/img/logo.png" alt="Logo" /></Link>
+                </picture>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
