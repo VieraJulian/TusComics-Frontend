1237fae68c37666486f2dded9dd076484838f6d3
VieraJulian <julianviera2019@hotmail.com>
2022-12-16 18:02:44 -0300

    Login.css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,18 @@
+import '../../public/css/Login-mobile.css'
+
 function Login() {
     return (
         <>
+            <div className="login-container">
+                <h1 className='login-h1'>Ingresá</h1>
+                <form className="form-login">
+                    <label>E-mail:</label>
+                    <input type="text" />
+                    <label>Password:</label>
+                    <input type="password" />
+                    <button className="boton-login" type="submit">Enviar</button>
+                </form>
+            </div>
         </>
     )
 }
