f3a8f86b6336babb64fae9775c3954b43c3f421e
VieraJulian <julianviera2019@hotmail.com>
2023-01-20 17:43:27 -0300

    Navbar terminado

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -22,25 +22,45 @@ function Navbar() {
         })
     }
 
-    let header = userLogged === null ? <div><Link to="/login">LOGIN</Link><span>|</span><Link to="/register">REGISTRARSE</Link></div> : <Link to="#" onClick={logout}>LOGOUT</Link>
+    let headerTop = userLogged === null ? <div><Link to="/login">LOGIN</Link><span>|</span><Link to="/register">REGISTRARSE</Link></div> : <Link to="#" onClick={logout}>LOGOUT</Link>
+    let notUser = <div>
+        <Link to="/">INICIO</Link>
+        <Link to="/comics">COMICS</Link>
+        <Link to="/collectibles">FIGURAS</Link></div>
+    let userLogin = <div>
+        <Link to="/">INICIO</Link>
+        <Link to="/comics">COMICS</Link>
+        <Link to="/collectibles">FIGURAS</Link>
+        <Link to="/profile"><i className="fa-solid fa-user"></i></Link></div>
+    let userAdmin = <div>
+        <Link to="/">INICIO</Link>
+        <Link to="/comics">COMICS</Link>
+        <Link to="/collectibles">FIGURAS</Link>
+        <Link to="/profile"><i className="fa-solid fa-user"></i></Link>
+        <Link to="/newProduct"><i className="fa-solid fa-circle-plus"></i></Link>
+        <Link to="/editProduct"><i className="fa-solid fa-pen-to-square"></i></Link></div>
 
-    console.log(userLogged)
+    let userN;
+
+    if (!userLogged) {
+        userN = notUser
+    } else if (userLogged && userLogged.admin === false) {
+        userN = userLogin
+    } else if (userLogged && userLogged.admin === true) {
+        userN = userAdmin
+    }
+
     return (
         <>
             <header>
                 <div className='first-div'>
-                    {header}
+                    {headerTop}
                 </div>
                 <picture className='header-picture'>
                     <img src="/public/img/logo.png" alt="Logo" />
                 </picture>
                 <nav className='header-nav'>
-                    <Link to="/">INICIO</Link>
-                    <Link to="/comics">COMICS</Link>
-                    <Link to="/collectibles">FIGURAS</Link>
-                    <Link to="/profile"><i className="fa-solid fa-user"></i></Link>
-                    <Link to="/newProduct"><i className="fa-solid fa-circle-plus"></i></Link>
-                    <Link to="/editProduct"><i className="fa-solid fa-pen-to-square"></i></Link>
+                    {userN}
                     <div className='cart-container'>
                         <Link to="/cart"><i className="fa-solid fa-cart-shopping"></i></Link><span className='cart-number'>0</span>
                     </div>
